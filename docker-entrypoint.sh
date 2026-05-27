5f433beedff369d755008264b167c16e55e8f739
apin <apin@urfit.com.tw>
2026-05-27 22:55:01 +0800

    fix: use sed to replace __ placeholders instead of envsubst

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-envsubst '${SUPABASE_URL} ${SUPABASE_ANON_KEY}' < /usr/share/nginx/html/recycler_onboarding.html > /tmp/recycler_onboarding.html
+sed -e "s|__SUPABASE_URL__|$SUPABASE_URL|g" \
+    -e "s|__SUPABASE_ANON_KEY__|$SUPABASE_ANON_KEY|g" \
+    /usr/share/nginx/html/recycler_onboarding.html > /tmp/recycler_onboarding.html
 mv /tmp/recycler_onboarding.html /usr/share/nginx/html/recycler_onboarding.html
 exec nginx -g 'daemon off;'
